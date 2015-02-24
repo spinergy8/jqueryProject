1c3d21a6725cc60a7972074f680bd52a8f76204a
S Clubb <spinergy_@hotmail.com>
2015-02-23 21:17:36 -0800

    first commit adding turtle game

---
+++ turtle.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+  $('button').click(function() {
+    //$('#Tobias').fadeIn("fast");
+    //$('#Wazkelly').fadeIn("fast");
+    
+
+
+var Animal = function(n, s, f) {
+  this.name = n;
+  this.speed = s;
+  this.focus = f;
+  this.position = 0;
+  this.report = function() {
+    return this.name + " is at " + this.position;
+  };
+
+  this.run = function() {
+    if (this.focus > Math.random() * 10) {
+      this.position += this.speed;
+    }
+  }
+  this.notYetWon = function() {
+    return this.position < distance;      
+  }     
+};
+
+var turtle = new Animal ("Tobias Turtle", 4, 5);
+    rabbit = new Animal ("Wazkelly Rabbit", 4, 5);
+    //rabbit.opacity = 1;
+    //turtle.opacity = 1;
+
+var distance = 55;
+var count = 0;
+
+while(turtle.notYetWon() && rabbit.notYetWon()){
+      turtle.run();
+
+if(turtle.position > distance/2 && turtle.position < distance/2 + 5 && count === 0) {
+  var booster = prompt(turtle.name + " is halfway there! Help him with a booster? (yes or no)");
+    if (booster.toUpperCase() == "YES") {
+      turtle.position += Math.ceil(Math.random() * 20);
+    }
+      else if (booster.toUpperCase() === "NO") {
+        turtle.position += 0;
+        count += 1
+        
+      }   
+}    
+      rabbit.run();
+
+if(rabbit.position > distance/2 && rabbit.position < distance/2 + 5 && count === 0) {
+  var booster = prompt(rabbit.name + " is halfway there! Help him with a booster? (yes or no)");
+    if (booster.toUpperCase() == "YES") {
+      rabbit.position += Math.ceil(Math.random() * 25);
+    }
+      else if (booster.toUpperCase() === "NO") {
+        rabbit.position += 0;
+        count += 1      
+      }   
+    }      
+};
+  var tresult = turtle.position;
+  var rresult = rabbit.position;
+  
+$(document).ready(function() {
+  $('p').text(function() {
+    if (tresult < rresult) {
+      $('#Tobias').fadeTo("fast",.25,function() {});
+      return("Wazkelly wins! Hopping all the way!")
+
+      console.log(rabbit);
+}
+  else { 
+    $('#Wazkelly').fadeTo("fast",.25,function() {});
+    return("Tobias wins! Slow and steady all the way!");
+    console.log(turtle);
+  };
+  });
+
+  });
+});
+});
+
+//console.log(turtle.report());
+//console.log(rabbit.report());
+
+
+
+
